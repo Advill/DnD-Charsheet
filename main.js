8452c27ef600be61b02a12812a257f5fa9554720
Peter Shumate <shumatpf@dukes.jmu.edu>
2018-10-04 17:13:20 -0400

    add style to buttons on spell sheet

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ function createWindow() {
     });
 
     win.on('closed', () => {
-        console.log('shutting down');
+        console.log('client shutting down');
         ch.send({message: 'shutdown'});
         win = null;
     });
